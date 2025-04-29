75eeba5548e588014698a2f6c455c0ebf2ac84be
GhoriHoly <ghoriholy@gmail.com>
2025-04-29 15:08:30 +0200

    Maven clean build

---
+++ githubaction/src/main/java/com/example/githubaction/Controller.java
@@ -0,0 +1,15 @@
+package com.example.githubaction;
+
+public class Controller {
+    public int myMethod(int a, int b) {
+        return a + b;
+    }
+
+    public double division(double a, double b) {
+
+        //lägger till massa cool kod
+
+
+        return a / b;
+    }
+}
